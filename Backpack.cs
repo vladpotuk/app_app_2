3ad1b7f5b4c157adf6b9c59c0accadb8c768c532
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 19:59:05 +0200

    update

---
+++ Backpack.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class Backpack
+{
+    public string Color { get; set; }
+    public string Brand { get; set; }
+    public string Material { get; set; }
+    public double Weight { get; set; }
+    public double Volume { get; set; }
+    public List<Item> Contents { get; set; } = new List<Item>();
+
+    public delegate void ItemAddedEventHandler(object sender, ItemAddedEventArgs e);
+    public event ItemAddedEventHandler ItemAdded;
+
+    public void AddItem(Item item)
+    {
+        if (CalculateOccupiedVolume() + item.Volume > Volume)
+        {
+            throw new InvalidOperationException("Cannot add item. Backpack volume exceeded.");
+        }
+
+        Contents.Add(item);
+        OnItemAdded(new ItemAddedEventArgs(item));
+    }
+
+    protected virtual void OnItemAdded(ItemAddedEventArgs e)
+    {
+        ItemAdded?.Invoke(this, e);
+    }
+
+    private double CalculateOccupiedVolume()
+    {
+        double occupiedVolume = 0;
+        foreach (var item in Contents)
+        {
+            occupiedVolume += item.Volume;
+        }
+        return occupiedVolume;
+    }
+}
+
+public class Item
+{
+    public string Name { get; set; }
+    public double Volume { get; set; }
+
+    public Item(string name, double volume)
+    {
+        Name = name;
+        Volume = volume;
+    }
+}
+
+public class ItemAddedEventArgs : EventArgs
+{
+    public Item AddedItem { get; }
+
+    public ItemAddedEventArgs(Item item)
+    {
+        AddedItem = item;
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Backpack backpack = new Backpack
+        {
+            Color = "Black",
+            Brand = "Nike",
+            Material = "Polyester",
+            Weight = 0.5,
+            Volume = 20
+        };
+
+        backpack.ItemAdded += (sender, e) =>
+        {
+            Console.WriteLine($"Item '{e.AddedItem.Name}' added to the backpack.");
+        };
+
+        try
+        {
+            backpack.AddItem(new Item("Water Bottle", 1.0));
+            backpack.AddItem(new Item("Book", 2.0));
+            backpack.AddItem(new Item("Laptop", 3.0));
+        }
+        catch (InvalidOperationException ex)
+        {
+            Console.WriteLine($"Error: {ex.Message}");
+        }
+    }
+}
