8c9bba668e07ac78a0d4c056a40b465c55ceb68a
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 19:05:44 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LambdaExpressionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Func<DateTime, bool> isProgrammerDay = date => date.DayOfYear == 256;
+
+            
+            DateTime testDate1 = new DateTime(2024, 9, 12); 
+            Console.WriteLine($"Дата {testDate1.ToShortDateString()} - день програміста: {isProgrammerDay(testDate1)}");
+
+            DateTime testDate2 = new DateTime(2024, 9, 13); 
+            Console.WriteLine($"Дата {testDate2.ToShortDateString()} - день програміста: {isProgrammerDay(testDate2)}");
+        }
+    }
+}
