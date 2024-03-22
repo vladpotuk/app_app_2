28da2c32e471ade9694d13a0fc5dd35f931b88e6
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 20:04:22 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int[] numbers = { -5, 3, -2, 7, 0, -1, 8, -4 };
+
+       
+        Func<int[], int> countPositiveNumbers = arr => arr.Count(x => x > 0);
+
+       
+        int count = countPositiveNumbers(numbers);
+        Console.WriteLine($"Кількість позитивних чисел у масиві: {count}");
+    }
+}
