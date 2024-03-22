656564e3cbcf62c0548e118448f215c5659caf16
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 20:01:55 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        int[] numbers = { 14, 21, 30, 35, 42, 49, 56, 63, 70 };
+
+        
+        Func<int[], int> countMultiplesOfSeven = arr => arr.Count(x => x % 7 == 0);
+
+        
+        int count = countMultiplesOfSeven(numbers);
+        Console.WriteLine($"Кількість чисел у масиві, кратних семи: {count}");
+    }
+}
