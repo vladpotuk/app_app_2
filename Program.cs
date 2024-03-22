8e3abd3f44ad472861009fe1c2789a179cc5785e
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 18:48:43 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DelegateEventExample
+{
+    class Program
+    {
+        
+        delegate bool CheckEvenDelegate(int number);
+
+        static void Main(string[] args)
+        {
+            
+            CheckEvenDelegate checkEven = num => num % 2 == 0;
+
+            
+            int testNumber = 10;
+            Console.WriteLine($"Число {testNumber} парне: {checkEven(testNumber)}");
+
+            testNumber = 7;
+            Console.WriteLine($"Число {testNumber} парне: {checkEven(testNumber)}");
+        }
+    }
+}
