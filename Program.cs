db8d8dfa66178b39f26b4c9e2515b1e216f92744
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 20:42:09 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,19 +1,32 @@
 ﻿using System;
-using System.Linq;
+using System.Collections.Generic;
 
 class Program
 {
-    
     static void Main(string[] args)
     {
-        
         int[] numbers = { -5, 3, -2, 7, 0, -1, 8, -4, -5, -2, 3 };
 
-        
-        Func<int[], IEnumerable<int>> uniqueNegativeNumbers = arr => arr.Where(x => x < 0).Distinct();
+       
+        Func<int[], IEnumerable<int>> uniqueNegativeNumbers = arr =>
+        {
+            List<int> uniqueNegatives = new List<int>();
+
+            foreach (int num in arr)
+            {
+                if (num < 0 && !uniqueNegatives.Contains(num))
+                {
+                    uniqueNegatives.Add(num);
+                }
+            }
+
+            return uniqueNegatives;
+        };
 
         
         var uniqueNegatives = uniqueNegativeNumbers(numbers);
+
+        
         Console.WriteLine("Унікальні, негативні числа у масиві:");
         foreach (var num in uniqueNegatives)
         {
