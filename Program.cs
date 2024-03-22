0284e8e8fb4d9fec5548173e84d7fa2e3c5fe70e
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 20:18:28 +0200

    opdate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+class Program
+{
+    
+    static void Main(string[] args)
+    {
+        
+        int[] numbers = { -5, 3, -2, 7, 0, -1, 8, -4, -5, -2, 3 };
+
+        
+        Func<int[], IEnumerable<int>> uniqueNegativeNumbers = arr => arr.Where(x => x < 0).Distinct();
+
+        
+        var uniqueNegatives = uniqueNegativeNumbers(numbers);
+        Console.WriteLine("Унікальні, негативні числа у масиві:");
+        foreach (var num in uniqueNegatives)
+        {
+            Console.WriteLine(num);
+        }
+    }
+}
