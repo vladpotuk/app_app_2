0fe124dd225a37aee4e2ce52a4a376901181b489
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 19:18:21 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Linq;
+
+namespace LambdaExpressionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Func<int[], int[]> findOddNumbers = array => array.Where(num => num % 2 != 0).ToArray();
+
+            int[] testArray = { 3, 7, 2, 9, 5, 6, 8 };
+            int[] oddNumbers = findOddNumbers(testArray);
+
+            Console.WriteLine("Непарні числа в масиві:");
+            foreach (int num in oddNumbers)
+            {
+                Console.WriteLine(num);
+            }
+        }
+    }
+}
