ff5c2b64a6c67ffbbedaea834b3792bff9ad64e8
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 19:14:22 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,26 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LambdaExpressionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<int[], int> findMin = array =>
+            {
+                int min = array[0]; 
+
+                foreach (int num in array)
+                {
+                    if (num < min)
+                        min = num; 
+                }
+
+                return min;
+            };
+
+            int[] testArray = { 3, 7, 2, 9, 5 };
+            Console.WriteLine($"Мінімум в масиві: {findMin(testArray)}");
+        }
+    }
+}
