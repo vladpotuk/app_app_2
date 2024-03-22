a70934394e727eef8574ac91c9d0b5873a546881
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 19:49:58 +0200

    opdate

--- Program.cs
+++ Program.cs
@@ -1,2 +1,48 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace RainbowColors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+          
+            Func<string, string> getColorName = color =>
+            {
+                
+                switch (color.ToLower())
+                {
+                    case "red":
+                        return "Red";
+                    case "orange":
+                        return "Orange";
+                    case "yellow":
+                        return "Yellow";
+                    case "green":
+                        return "Green";
+                    case "blue":
+                        return "Blue";
+                    case "indigo":
+                        return "Indigo";
+                    case "violet":
+                        return "Violet";
+                    default:
+                        return "Unknown color";
+                }
+            };
+
+            
+            string color = "orange";
+            string colorName = getColorName(color);
+            Console.WriteLine($"Назва кольору для кольору {color}: {colorName}");
+
+            color = "green";
+            colorName = getColorName(color);
+            Console.WriteLine($"Назва кольору для кольору {color}: {colorName}");
+
+            color = "indigo"; 
+            colorName = getColorName(color);
+            Console.WriteLine($"Назва кольору для кольору {color}: {colorName}");
+        }
+    }
+}
