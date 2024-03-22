a5c2ca93189ca612dd82e86fd416af92013f8c8f
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 19:00:43 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LambdaExpressionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            Func<int, int> cube = num => num * num * num;
+
+           
+            int testNumber = 3;
+            Console.WriteLine($"Куб числа {testNumber} дорівнює: {cube(testNumber)}");
+
+            testNumber = 5;
+            Console.WriteLine($"Куб числа {testNumber} дорівнює: {cube(testNumber)}");
+        }
+    }
+}
