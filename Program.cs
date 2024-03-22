fec3ff6f2360dceecc7cc34dc6bb7f1b7491d953
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 20:44:06 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        
+        string text = "Це приклад тексту для перевірки роботи лямбди.";
+
+       
+        string wordToFind = "перевірки";
+
+        
+        Func<string, string, bool> wordChecker = (inputText, word) =>
+            inputText.ToLower().Contains(word.ToLower());
+
+        
+        bool containsWord = wordChecker(text, wordToFind);
+        Console.WriteLine($"Текст містить слово '{wordToFind}': {containsWord}");
+    }
+}
