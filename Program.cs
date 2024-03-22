0f486509dca65cab2ee9f6d083e259bd8f6b7afe
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 19:11:11 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,28 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace LambdaExpressionExample
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Func<int[], int> findMax = array =>
+            {
+                int max = array[0]; 
+
+                foreach (int num in array)
+                {
+                    if (num > max)
+                        max = num; 
+                }
+
+                return max;
+            };
+
+            
+            int[] testArray = { 3, 7, 2, 9, 5 };
+            Console.WriteLine($"Максимум в масиві: {findMax(testArray)}");
+        }
+    }
+}
