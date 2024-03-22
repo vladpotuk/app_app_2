8d39ae45c9f763745c0dcbf499e977b881cf7d37
vladpotuk <vladpotuk01@gmail.com>
2024-03-22 18:53:30 +0200

    update

--- Program.cs
+++ Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+namespace DelegateEventExample
+{
+    class Program
+    {
+        
+        delegate int SquareDelegate(int number);
+
+        static void Main(string[] args)
+        {
+            
+            SquareDelegate square = num => num * num;
+
+            
+            Console.Write("Введіть число: ");
+            int userNumber = Convert.ToInt32(Console.ReadLine());
+
+            
+            Console.WriteLine($"Квадрат числа {userNumber} дорівнює: {square(userNumber)}");
+        }
+    }
+}
